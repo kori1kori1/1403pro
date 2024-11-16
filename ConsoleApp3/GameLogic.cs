6092661dcbd67a8c15d0164398833f5857b28ff8
kori1kori1 <kourosh.rezaei2004@gmail.com>
2024-11-16 17:57:18 +0330

    Add files via upload

---
+++ ConsoleApp3/GameLogic.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace ConsoleApp3;
+
+static class KeyBoard
+{
+    public static char GetYesOrNo()
+    {
+        char key;
+        do
+        {
+            key = Console.ReadKey(true).KeyChar;
+        } while (key != 'y' && key != 'n');
+        Console.WriteLine(key);
+        return key;
+    }
+}
+
+class NumberGuesser
+{
+    private int min, max;
+
+    public NumberGuesser(int start, int end)
+    {
+        min = start;
+        max = end;
+    }
+
+    public int Guess => (min + max) / 2;
+
+    public void AdjustRange(bool isHigher)
+    {
+        if (isHigher) min = Guess + 1;
+        else max = Guess - 1;
+    }
+
+    public bool IsValidRange => min <= max;
+}
+
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp3;
+
+Console.WriteLine("Please choose a number between 1 and 100 in your mind.");
+var guesser = new NumberGuesser(1, 100);
+
+while (true)
+{
+    Console.WriteLine($"your number is  {guesser.Guess} ? (y/n)");
+    if (KeyBoard.GetYesOrNo() == 'y')
+    {
+        Console.WriteLine("I guessed your number!");
+        break;
+    }
+
+    Console.WriteLine("Is your number bigger? (y/n)");
+    guesser.AdjustRange(KeyBoard.GetYesOrNo() == 'y');
+
+    if (!guesser.IsValidRange)
+    {
+        Console.WriteLine("Incorrect information has been entered!");
+        break;
+    }
+}
+
+Console.WriteLine(" the end!");
